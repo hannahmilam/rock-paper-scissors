7ddcbf88b7f9199ae939bfc9b9272760e89f66fe
Hannah Milam <hannah.milam16@gmail.com>
2021-08-25 21:19:13 -0600

    updated icon

--- app/main.js
+++ app/main.js
@@ -1,57 +1,78 @@
-const possibleChoices = ["rock", "paper", "scissors"]
-let outcome = document.getElementById("outcome")
+let possibleChoices = ["rock", "paper", "scissors"]
 let currentPlayer = {}
-
+let outcome = document.getElementById("outcome")
+let playerSelection = document.getElementById("playerSelection")
 
 function play(playerChoice){
    let computerChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
    assignCompChoice(computerChoice)
 
-    if(playerChoice === computerChoice){
-        outcome.innerText="TIE!"
-        return console.log("tie!")
-    }
     if(playerChoice === "rock" && computerChoice === "scissors"){
+        playerSelection.innerHTML=`<i class="far fa-hand-rock"></i>`
         outcome.innerText="YOU WIN! :)"
         return console.log("you win!")
     }
     if(playerChoice === "rock" && computerChoice === "paper"){
+        playerSelection.innerHTML=`<i class="far fa-hand-rock"></i>`
         outcome.innerText="YOU LOSE! :("
         return console.log("you lose!")
     }
     if(playerChoice === "paper" && computerChoice === "rock"){
+        playerSelection.innerHTML=`<i class="far fa-hand-paper"></i>`
         outcome.innerText="YOU WIN! :)"
         return console.log("you win!")
     }
     if(playerChoice === "paper" && computerChoice === "scissors"){
+        playerSelection.innerHTML=`<i class="far fa-hand-paper"></i>`
         outcome.innerText="YOU LOSE! :("
         return console.log("you lose!")
     }
     if(playerChoice === "scissors" && computerChoice === "paper"){
+        playerSelection.innerHTML=`<i class="far fa-hand-scissors"></i>`
         outcome.innerText="YOU WIN! :)"
         return console.log("you win!")
     }
     if(playerChoice === "scissors" && computerChoice === "rock"){
+        playerSelection.innerHTML=`<i class="far fa-hand-scissors"></i>`
         outcome.innerText="YOU LOSE! :("
         return console.log("you lose!")
+    } else {
+        outcome.innerText="TIE!"
+        return console.log("tie!")
     }
-}
+    }
 
-function assignCompChoice(computerChoice){
-    const compChoiceElem = document.getElementById("compChoice")
-    if(computerChoice === "rock"){
+function assignCompChoice(compChoice){
+    let compChoiceElem = document.getElementById("compChoice")
+    if(compChoice === "rock"){
         compChoiceElem.innerHTML=`<i class="far fa-hand-rock"></i>`
         return console.log("rock")
     }
-    if(computerChoice === "paper"){
+    if(compChoice === "paper"){
         compChoiceElem.innerHTML=`<i class="far fa-hand-paper"></i>`
-        return console.log('paper')
-    }
-    if(computerChoice === "scissors"){
+        return console.log("rock")
+    }if(compChoice === "scissors"){
         compChoiceElem.innerHTML=`<i class="far fa-hand-scissors"></i>`
-        return console.log('scissors')
+        return console.log("rock")
     }
 }
+
+function assignPlayerSelection(playerSelection){
+    let playerSelectionElem = document.getElementById("playerSelection")
+    if(playerSelection === "rock"){
+        playerSelectionElem.innerHTML=`<i class="far fa-hand-rock"></i>`
+        return console.log("rock")
+    }
+    if(playerSelection === "paper"){
+        playerSelectionElem.innerHTML=`<i class="far fa-hand-paper"></i>`
+        return console.log("paper")
+    }
+    if(playerSelection === "scissors"){
+        playerSelectionElem.innerHTML=`<i class="far fa-hand-scissors"></i>`
+        return console.log("scissors")
+    }
+}
+
 
 function setPlayer(event){
     event.preventDefault()
@@ -60,10 +81,3 @@ function setPlayer(event){
     let playerName = form.playerName.value
 }
 
-function storePlayer(){
-
-}
-
-function endGame(){
-
-}
